1ed430635c7648de28a3185df1930c8193ba13f6
unknown <toghrul@code.edu.az>
2022-08-03 18:22:40 +0400

    .

---
+++ pro.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string metin = "Code academy";
+            for (int i = 0; i < metin.Length; i++)
+                if (metin[i] == 'a')
+                {
+                    Console.WriteLine("a herfi var");
+                    break; }
+                    else
+                    {
+                        
+                            Console.WriteLine("a herfi yoxdur");
+                        break;
+                    }
+                        
+                }
+            
+
+            }
+        }
+    
+
